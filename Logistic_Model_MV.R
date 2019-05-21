b33010a980344dcd205bd5521ed67bf0dc6dbd88
matthew.vincent <matthew.vincent88@gmail.com>
2019-05-21 18:25:44 +0930

    Update Logistic_Model_MV.R

--- Logistic_Model_MV.R
+++ Logistic_Model_MV.R
@@ -1,12 +1,26 @@
 
+#################     SM3 GROUP ASSIGNMENT    ##################
+
+################################################################
+
+
 # Set working directory
-setwd("E:\Documents\GitHub\SM3_Group_Project")
+setwd("E:/Documents/GitHub/SM3_Group_Project")
+
+# Import libraries
+library(caret)
+
+
+
+#################     FUNCTION DEFINITIONS    ##################
 
 # Function to load the data in file (e.g. "data.csv"), with the 
 # response variable in the last column
 # Returns validation data set of size N, train data set with the rest of the data
 loadData = function(file = "train.csv", N = 1000) {
 
+  head(file)
+  
   dat = read.csv(file)
   validInd = sample(1:nrow(dat), N) 
   train = dat[-validInd, ]
@@ -18,4 +32,67 @@ loadData = function(file = "train.csv", N = 1000) {
   
   return(list(trainX, trainY, validX, validY))
 }
+
+# Test loadData function and store output
+output <- loadData()
+
+
+
+# Function to train our logistic regression model
+train_logit_model = function(data) {
+
+  # Need to understand logit model hyperparamters better
+  
+  # Then need to determine the best way in R of generating 
+  # and evaluating different models with different hyperparameters
+  
+  # The output of this function will be the logistic regression 
+  # coefficients 
+  
+  return(0)
+  
+}
+
+logit_prediction = function(predictors) {
+  
+  # Use model that we trained to make predictions 
+  predictions <- predict(logit_model,predictors,type="response")
+  return(0)
+  
+}
+
+
+   
+#################            DRIVER             ##################
+
+# Read in headers
+headers <- read.csv(file = "train.csv", skip = 1, header = F, nrows = 1, as.is = T)
+
+# Read in complete dataset
+dat <- read.csv(file = "train.csv", skip = 2, header = F)
+
+# Add the headers to dataset
+colnames(dat) <- headers
+
+trainY = dat[ ,ncol(dat)]
+
+# Generate a logistic regression model
+logit_model <- glm(trainY ~ Sex, data = full_set, family = binomial(logit))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
