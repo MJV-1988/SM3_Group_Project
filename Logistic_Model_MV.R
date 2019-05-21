df82b2a39967e9ed1666dba2dcd5c8c53d00583e
matthew.vincent <matthew.vincent88@gmail.com>
2019-05-21 14:16:45 +0930

    Create Logistic_Model_MV.R

---
+++ Logistic_Model_MV.R
@@ -0,0 +1,21 @@
+
+# Set working directory
+setwd("E:\Documents\GitHub\SM3_Group_Project")
+
+# Function to load the data in file (e.g. "data.csv"), with the 
+# response variable in the last column
+# Returns validation data set of size N, train data set with the rest of the data
+loadData = function(file = "train.csv", N = 1000) {
+
+  dat = read.csv(file)
+  validInd = sample(1:nrow(dat), N) 
+  train = dat[-validInd, ]
+  valid = dat[validInd, ]
+  trainX = train[ ,1:(ncol(dat)-1)]
+  trainY = train[ ,ncol(dat)]
+  validX = valid[ ,1:(ncol(dat)-1)]
+  validY = valid[ ,ncol(dat)]
+  
+  return(list(trainX, trainY, validX, validY))
+}
+
